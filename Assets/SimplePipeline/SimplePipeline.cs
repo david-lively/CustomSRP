c200e59fc83e4860e74d774023a9b4868a664393
David Lively <davidlively@gmail.com>
2019-11-17 14:45:12 -0600

    attempting to get this working on both PCs

--- Assets/SimplePipeline/SimplePipeline.cs
+++ Assets/SimplePipeline/SimplePipeline.cs
@@ -8,14 +8,27 @@ public class SimplePipeline : RenderPipeline
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
         ScriptableCullingParameters cullParams;
+        var clearColor = new Color(0, 0, 0.1f);
         foreach (var camera in cameras)
         {
+            var buffer = new CommandBuffer();
+
             context.SetupCameraProperties(camera);
+
+            buffer.ClearRenderTarget(true, true, clearColor);
+            context.ExecuteCommandBuffer(buffer);
+            buffer.Release();
+
             if (!camera.TryGetCullingParameters(out cullParams))
                 continue;
+
+            var cullResults = context.Cull(ref cullParams);
+
+            var drawSettings = new DrawingSettings();
+            var filterSettings = new FilteringSettings();
+
+            context.DrawRenderers(cullResults, ref drawSettings, ref filterSettings);
         }
-
-
 
         context.Submit();
     }
