d0ac20b6d9cace7c4224c3919b565eac40bb88f4
David Lively <davidlively@gmail.com>
2019-11-17 16:18:34 -0600

    Added rendering for unlit, transparent and incompatible objects (with magenta error shader)

--- Assets/SimplePipeline/SimplePipeline.cs
+++ Assets/SimplePipeline/SimplePipeline.cs
@@ -5,31 +5,83 @@ using UnityEngine.Rendering;
 
 public class SimplePipeline : RenderPipeline
 {
+    Material errorMaterial;
+    /*
+     * Draw any objects with incompatible materials with magenta error shader
+     */
+    private void DrawWithDefaultPipeline(ScriptableRenderContext context, Camera camera, CullingResults cullResults)
+    {
+        if (null == errorMaterial)
+        {
+            var errorShader = Shader.Find("Hidden/InternalErrorShader");
+            errorMaterial = new Material(errorShader) { hideFlags = HideFlags.HideAndDontSave };
+
+        }
+
+        var drawSettings = new DrawingSettings(new ShaderTagId("ForwardBase"), new SortingSettings(camera));
+        drawSettings.overrideMaterial = errorMaterial;
+
+        var filterSettings = FilteringSettings.defaultValue;
+
+        context.DrawRenderers(cullResults, ref drawSettings, ref filterSettings);
+    }
+
+
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
+        BeginFrameRendering(context, cameras);
         ScriptableCullingParameters cullParams;
         var clearColor = new Color(0, 0, 0.1f);
         foreach (var camera in cameras)
         {
-            var buffer = new CommandBuffer();
-
-            context.SetupCameraProperties(camera);
-
-            buffer.ClearRenderTarget(true, true, clearColor);
-            context.ExecuteCommandBuffer(buffer);
-            buffer.Release();
-
             if (!camera.TryGetCullingParameters(out cullParams))
                 continue;
 
+            // notify engine and plugins that this camera is starting to render.
+            BeginCameraRendering(context, camera);
+
+            // configure view matrix, clipping, etc.
+            context.SetupCameraProperties(camera);
+            {
+                var buffer = new CommandBuffer();
+                buffer.ClearRenderTarget(true, true, clearColor);
+                context.ExecuteCommandBuffer(buffer);
+                buffer.Release();
+            }
+            // cull for this camera
             var cullResults = context.Cull(ref cullParams);
 
-            var drawSettings = new DrawingSettings();
-            var filterSettings = new FilteringSettings();
 
+
+            // Draw opaque objects
+            var drawSettings = new DrawingSettings(new ShaderTagId("SRPDefaultUnlit"), new SortingSettings(camera));
+            //drawSettings.sortingSettings = new SortingSettings(camera);
+
+            var filterSettings = FilteringSettings.defaultValue;
+            filterSettings.renderQueueRange = RenderQueueRange.opaque;
             context.DrawRenderers(cullResults, ref drawSettings, ref filterSettings);
+
+            // Draw skybox
+            context.DrawSkybox(camera);
+
+
+            // Draw transparent objects
+            drawSettings.sortingSettings = new SortingSettings(camera) { criteria = SortingCriteria.CommonTransparent };
+            filterSettings.renderQueueRange = RenderQueueRange.transparent;
+            context.DrawRenderers(cullResults, ref drawSettings, ref filterSettings);
+
+            /*
+             * draw anything that's left. Materials are mutually exclusive between SRP and built-in
+             * pipelines, so this will only draw objects that have not already been drawn.
+             */
+            DrawWithDefaultPipeline(context, camera, cullResults);
+
+            // Notify engine, plugins, etc. that this camera is finished rendering.
+            EndCameraRendering(context, camera);
+
         }
 
         context.Submit();
+        EndFrameRendering(context, cameras);
     }
 }
